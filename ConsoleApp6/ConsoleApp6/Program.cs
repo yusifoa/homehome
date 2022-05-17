7b1094e554cf112d8c6244a5b82cd513087087bf
Yusif O. Abilov <yusifoa@code.local>
2022-05-17 18:55:22 +0400

    Add homework

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nurlan Faiq  Emil Tural Yusif";
+            Console.WriteLine(ReturnString("Nurlan Faiq  Emil Tural Yusif"));
+        }
+
+       static string ReturnString(string name)
+        {
+            string[] namearray = name.Split(' ');
+            string b = "";
+            foreach (var item in namearray)
+            {
+
+                for (int i = item.Length - 1; i >=  0; i--)
+                {
+                    b = b + item[i];
+                }
+                b = b + " ";
+            }
+            return b;
+        }
+    }
+}
